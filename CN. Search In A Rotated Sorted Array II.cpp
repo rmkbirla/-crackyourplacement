790007b110e3ee97a2d5bfaf8bb755977bc8a5bf
Ramkrishna Birla <rmkbirla@gmail.com>
2023-07-31 15:27:42 +0530

    Create CN. Search In A Rotated Sorted Array II.cpp
    other that #crackyourplacement

---
+++ CN. Search In A Rotated Sorted Array II.cpp
@@ -0,0 +1,32 @@
+bool searchInARotatedSortedArrayII(vector<int>&nums, int target) {
+   
+   int low=0;
+        int high=nums.size()-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(target==nums[mid]){
+                return true;
+            }
+            if(nums[low]==nums[mid] && nums[mid]==nums[high]){
+                low++;
+                high--;
+                continue;
+            }
+            if(nums[low]<=nums[mid]){
+                if(nums[low]<=target && target<=nums[mid]){
+                    high=mid-1;
+                }else{
+                    low=mid+1;
+                }
+            }
+            else{
+                if(nums[mid]<=target && target<=nums[high]){
+                    low=mid+1;
+                }else{
+                    high=mid-1;
+                }
+            }
+        }
+        return false;
+    // Write your code here.
+}
